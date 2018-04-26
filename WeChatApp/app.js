8c615195cd592f660086a10198c561aed6ce9672
xpsilvester <xpsilvester@gmail.com>
2018-04-26 16:13:20 +0800

    add

---
+++ WeChatApp/app.js
@@ -0,0 +1,15 @@
+App({
+  onLaunch: function(options) { 
+    // Do something initial when launch.
+  },
+  onShow: function(options) {
+      // Do something when show.
+  },
+  onHide: function() {
+      // Do something when hide.
+  },
+  onError: function(msg) {
+    console.log(msg)
+  },
+  globalData: 'I am global data'
+})
---
+++ WeChatApp/pages/index/index.js
@@ -0,0 +1,39 @@
+//index.js
+Page({
+  data: {
+    text: 'init data',
+    num: 0,
+    array: [{text: 'init data'}],
+    object: {
+      text: 'init data'
+    }
+  },
+  changeText: function() {
+    // this.data.text = 'changed data'  // bad, it can not work
+    this.setData({
+      text: 'changed data'
+    })
+  },
+  changeNum: function() {
+    this.data.num = 1
+    this.setData({
+      num: this.data.num
+    })
+  },
+  changeItemInArray: function() {
+    // you can use this way to modify a danamic data path
+    this.setData({
+      'array[0].text':'changed data'
+    })
+  },
+  changeItemInObject: function(){
+    this.setData({
+      'object.text': 'changed data'
+    });
+  },
+  addNewField: function() {
+    this.setData({
+      'newField.text': 'new data'
+    })
+  }
+})
