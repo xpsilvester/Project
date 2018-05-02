77b26e33f8325a9b7879a52b8cae88afce8b3d34
xpsilvester <xpsilvester@gmail.com>
2018-05-02 16:11:48 +0800

    add

--- WeChatApp/pages/index/index.js
+++ WeChatApp/pages/index/index.js
@@ -1,39 +1,54 @@
 //index.js
+//获取应用实例
+const app = getApp()
+
 Page({
   data: {
-    text: 'init data',
-    num: 0,
-    array: [{text: 'init data'}],
-    object: {
-      text: 'init data'
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
     }
   },
-  changeText: function() {
-    // this.data.text = 'changed data'  // bad, it can not work
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
     this.setData({
-      text: 'changed data'
-    })
-  },
-  changeNum: function() {
-    this.data.num = 1
-    this.setData({
-      num: this.data.num
-    })
-  },
-  changeItemInArray: function() {
-    // you can use this way to modify a danamic data path
-    this.setData({
-      'array[0].text':'changed data'
-    })
-  },
-  changeItemInObject: function(){
-    this.setData({
-      'object.text': 'changed data'
-    });
-  },
-  addNewField: function() {
-    this.setData({
-      'newField.text': 'new data'
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   }
 })
