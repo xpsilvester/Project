63070bf050b822066844aba74d51dfccf183c2bf
xpsilvester <wuxp5@mail2.sysu.edu.cn>
2018-05-08 22:35:15 +0800

    add

--- WeChatApp/pages/index/index.js
+++ WeChatApp/pages/index/index.js
@@ -8,6 +8,7 @@ let proList=[{
   ProductName: '红米Note 5 AI双摄',
   CurrentPrice: 1399,
   BasicPrice: 1499,
+  className:'手机',
   ProductDescLong: [
     '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/63bcf275fd8393325c6f343b3c89e5c9.jpg?w=1080&h=1735',
     '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/bfb8f59814fa33003e94451eeaba0f9f.jpg?w=1080&h=1397',
@@ -24,6 +25,7 @@ let proList=[{
   ProductName: '小米6 变焦双摄',
   CurrentPrice: 2299,
   BasicPrice: 2499,
+  className: '手机',
   ProductDescLong: [
     '//i8.mifile.cn/v1/a1/80986f52-42e8-95cb-d420-4acc958a0aef.jpg?w=1080&h=1923&s=181.6',
     '//i8.mifile.cn/v1/a1/1f9cdfc0-3b38-a61b-2f7a-559f912384a2.jpg?w=1080&h=1349&s=181',
@@ -34,14 +36,56 @@ let proList=[{
     '//i8.mifile.cn/v1/a1/3c96e923-284b-0d4c-b7e3-e3e6acd713f1!720x792.webp',
     '//i8.mifile.cn/v1/a1/a25fd308-dce7-393e-3113-02d52b817d5d!720x792.webp',
   ]
-}];
+},{
+    SysNo: 321,
+    ProductPicSrc200: 'https://i1.mifile.cn/a1/pms_1519959193.42473450!220x220.jpg',
+    ProductName: '小米电视4A 40英寸',
+    CurrentPrice: 1399,
+    BasicPrice: 1599,
+    className: '家电',
+    ProductDescLong: [
+      '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f5ddb967ed32856fd26b8579b25da4fa.jpg?w=1080&h=1855',
+      '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/4be84c00f6a4ae597aba0001a818954b.jpg?w=1080&h=996',
+      '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/cae9e50679b04e3e45cba8c329e52d34.jpg?w=1080&h=1861'
+    ],
+    swiper: [
+      '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/396f4f9484abeb5f11352e1111084e4d.jpg?thumb=1&w=720&h=792',
+      '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/2312e48bd8e55e8b8595dc49f7ae358c.jpg?thumb=1&w=720&h=792'
+    ]
+}, {
+  SysNo: 521,
+  ProductPicSrc200: 'https://i1.mifile.cn/a1/pms_1499072633.96298268!220x220.jpg',
+  ProductName: '小米盒子3 增强版',
+  CurrentPrice: 399,
+  BasicPrice: 499,
+  className: '家电',
+  ProductDescLong: [
+    '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f5ddb967ed32856fd26b8579b25da4fa.jpg?w=1080&h=1855',
+    '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/4be84c00f6a4ae597aba0001a818954b.jpg?w=1080&h=996',
+    '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/cae9e50679b04e3e45cba8c329e52d34.jpg?w=1080&h=1861'
+  ],
+  swiper: [
+    '//i8.mifile.cn/v1/a1/8211c189-b600-91df-9b9f-8738c52c89e0!720x792.webp',
+    '//i8.mifile.cn/v1/a1/afcf9651-3d01-e9cb-eda4-579181a44f57!720x792.webp',
+    '//i8.mifile.cn/v1/a1/3fa86d00-8a00-7b82-5398-f90321614ffb!720x792.webp'
+  ]
+}
+];
+let classList = [{
+  "className": "手机",
+  "SysNos": '111,121'
+},
+{
+  "className": "家电",
+  "SysNos": '321,521'
+}]
 Page({
   data: {
     logo:'XP-SIL',
     logoTitle: 'XP-SIL商城',
     logoUrl:'/images/logo.png',
     showAll:'店铺全部商品',
-    category:'手机',
+    classList: classList,
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -54,8 +98,9 @@ Page({
   },
   //页面跳转
   toProList: function (e) {
+    var className = e.currentTarget.dataset.type;
     wx.navigateTo({
-      url: '/pages/products/list/list',
+      url: '/pages/products/list/list?className=' + className,
     })
   },
   toProDetail: function (e) {
--- WeChatApp/pages/products/list/list.js
+++ WeChatApp/pages/products/list/list.js
@@ -1,81 +1,53 @@
 // pages/products/list/list.js
-let proListup = [{
-  SysNo: 111,
-  ProductPicSrc200: '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/6efc52e32da7595519d9907cc124a50c.jpg?thumb=1&w=720&h=792',
-  ProductName: '红米Note 5 AI双摄',
-  CurrentPrice: 1399,
-  }
-  , {
-    SysNo: 121,
-    ProductPicSrc200: '//i8.mifile.cn/v1/a1/2928abd1-0e07-b1ba-59cc-9f3410cf7fde!720x792.webp',
-    ProductName: '小米6 变焦双摄',
-    CurrentPrice: 2299,
-}]
-let proListdown = [
-  {
-    SysNo: 121,
-    ProductPicSrc200: '//i8.mifile.cn/v1/a1/2928abd1-0e07-b1ba-59cc-9f3410cf7fde!720x792.webp',
-    ProductName: '小米6 变焦双摄',
-    CurrentPrice: 2299,
-  },{ 
-    SysNo: 111,
-    ProductPicSrc200: '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/6efc52e32da7595519d9907cc124a50c.jpg?thumb=1&w=720&h=792',
-    ProductName: '红米Note 5 AI双摄',
-    CurrentPrice: 1399,
-  }
-]
-let proListSale = [{
-  SysNo: 111,
-  ProductPicSrc200: '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/6efc52e32da7595519d9907cc124a50c.jpg?thumb=1&w=720&h=792',
-  ProductName: '红米Note 5 AI双摄',
-  CurrentPrice: 1399,
-  }
-  , {
-    SysNo: 121,
-    ProductPicSrc200: '//i8.mifile.cn/v1/a1/2928abd1-0e07-b1ba-59cc-9f3410cf7fde!720x792.webp',
-    ProductName: '小米6 变焦双摄',
-    CurrentPrice: 2299,
-}]
-let proListTime = [{
-  SysNo: 121,
-  ProductPicSrc200: '//i8.mifile.cn/v1/a1/2928abd1-0e07-b1ba-59cc-9f3410cf7fde!720x792.webp',
-  ProductName: '小米6 变焦双摄',
-  CurrentPrice: 2299,
-  }
-  , {
-    SysNo: 111,
-    ProductPicSrc200: '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/6efc52e32da7595519d9907cc124a50c.jpg?thumb=1&w=720&h=792',
-    ProductName: '红米Note 5 AI双摄',
-    CurrentPrice: 1399,
-}]
-let proList = wx.getStorageSync('proList');
+let proListup;
+let proListdown = [];
+let proListSale = [];
+let proListTime = [];
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title:'热销产品',
-    proList: proList,
-    tagNum:0,
-    order:'▲',
-    priceOrder:0
+    title: '全部',
+    tagNum: 0,
+    order: '▲',
+    priceOrder: 0
   },
-  price:function(){
-    let list,ord,pOrd;
-    var self=this;
-    if (this.data.priceOrder == 0){
+  //排序
+  sort: function (arr, key) {
+    var flag = 0;
+    do {
+      flag = 1;
+      for (var i = 0; i < arr.length - 1; i++) {
+        if (arr[i][key] > arr[i + 1][key]) {
+          var temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          flag = 0;
+        }
+      }
+    }
+    while (flag == 0)
+  },
+  //按价格
+  price: function () {
+    let list, ord, pOrd;
+    var self = this;
+    if (this.data.priceOrder == 0) {
       list = proListdown;
       ord = '▼';
       pOrd = 1;
-    } else if (this.data.priceOrder == 2){
-      list = this.data.proList;
+    } else if (this.data.priceOrder == 2) {
+      // list = this.data.proList;
       ord = this.data.order;
-      if (this.data.order == '▼'){
+      if (this.data.order == '▼') {
+        list = proListdown
         pOrd = 1;
-      }else{
+      } else {
+        list = proListup;
         pOrd = 0;
-      }      
+      }
     } else {
       list = proListup;
       ord = '▲';
@@ -98,13 +70,15 @@ Page({
       priceOrder: 0
     })
   },
-  sale:function(){
+  //按销量
+  sale: function () {
     this.setData({
       proList: proListSale,
       tagNum: 1,
       priceOrder: 2
     })
   },
+  //按时间
   time: function () {
     this.setData({
       proList: proListTime,
@@ -112,67 +86,84 @@ Page({
       priceOrder: 2
     })
   },
+  //页面跳转
   toProDetail: function (e) {
     var SysNo = e.currentTarget.dataset.id;
     wx.navigateTo({
-      url: '/pages/products/detail/detail?SysNo=' + SysNo,
+      url: '/pages/products/detail/detail?SysNo=' + SysNo
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let className = options.className || '全部';
     wx.setNavigationBarTitle({
-      title: '热销产品'
+      title: className
     })
+
+    let proList = wx.getStorageSync('proList');
+    proListdown = wx.getStorageSync('proList');
+    proListSale = wx.getStorageSync('proList');
+    proListTime = wx.getStorageSync('proList');
+    this.sort(proList, 'CurrentPrice');
+    proListup = proList;
+    this.setData({
+      proList: proList,
+      className: className
+    })
+    this.sort(proListdown, 'CurrentPrice');
+    proListdown.reverse();
+    this.sort(proListSale, 'SaleQty');
+    this.sort(proListTime, 'CreateTime');
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
