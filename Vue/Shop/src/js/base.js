eb1c0904bb9adc3f217a1f97c52b3ab7ea33abca
xpsilvester <xpsilvester@gmail.com>
2018-06-12 14:15:43 +0800

    add

--- Vue/Shop/src/js/base.js
+++ Vue/Shop/src/js/base.js
@@ -1,1 +1,8 @@
-
+$(function(){
+	$('.originSearch input').on('focus',function(){
+		$('.searchAdd').css('display','block');
+	})
+	$('.searchAdd .back').on('click',function(){
+		$('.searchAdd').css('display','none');
+	})
+})
