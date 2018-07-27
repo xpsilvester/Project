9a2973bd0e06ea8cb42f16e387bf69358f600a2c
xpsilvester <xpsilvester@gmail.com>
2018-07-27 14:45:33 +0800

    modify cart

--- Vue/Shop/src/pages/Cart.vue
+++ Vue/Shop/src/pages/Cart.vue
@@ -5,6 +5,24 @@
       <p>购物车还是空的~</p>
       <div @click="toHome">去逛逛</div>
     </div>
+    <div class="product">
+      <div class="choosed"></div>
+      <div class="img">
+        <img src="@/assets/cart_mi8.jpg" alt="" />
+      </div>
+      <div class="info">
+        <p class="name">小米8 全网通版 6GB内存 128GB 黑色</p>
+        <p class="price">售价：2999元</p>
+        <div class="num">
+          <div class="input-number">
+            <div class="input-sub"></div>
+            <div class="input-num">1</div>
+            <div class="input-add"></div>
+          </div>
+          <div class="delete"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
