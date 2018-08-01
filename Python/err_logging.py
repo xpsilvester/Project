bfce6e9d0891c23722acd203d105ff735c4c949e
xpsilvester <xpsilvester@gmail.com>
2018-08-01 09:36:29 +0800

    modify

---
+++ Python/err_logging.py
@@ -0,0 +1,16 @@
+import logging
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+main()
+print('END')
